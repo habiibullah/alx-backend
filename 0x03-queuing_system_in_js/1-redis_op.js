6577a131c71030d1e7cfa125fa4e649f304dc7a0
habiibullah <ajiboyhabiib@gmail.com>
2023-08-28 19:53:50 +0000

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+	client.SET(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+	client.GET(schoolName, (_err, reply) => {
+		console.log(reply);
+	});
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
